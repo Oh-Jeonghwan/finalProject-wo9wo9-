5fe36fe1dc711d73202bfe96a1d0883e66808e00
Jeonghwan Oh <vmflwmf@gmail.com>
2022-02-04 15:15:48 +0900

    20220204 15:15 first commit

---
+++ src/main/java/com/oneteam/wo9wo9/selfdo/model/dao/SelfdoDao.java
@@ -0,0 +1,23 @@
+package com.oneteam.wo9wo9.selfdo.model.dao;
+
+import java.util.List;
+
+import com.oneteam.wo9wo9.packagedo.model.vo.Basket;
+import com.oneteam.wo9wo9.selfdo.model.vo.Selfdo;
+import com.oneteam.wo9wo9.selfdo.model.vo.Sidedo;
+
+public interface SelfdoDao {
+	
+	List<Sidedo> sideList();
+
+	List<Sidedo> sidedoList1();
+
+	List<Sidedo> sidedoList2();
+
+	List<Sidedo> sidedoList3();
+
+	int putselfdo(Selfdo selfdo);
+
+	int putbasket(Basket basket);
+
+}
